b050d6ceb61f2f15be48d26a6d3a96c8cabbf841
Enrico Pagliara <enrico.pagliara@claninformatico.it>
2025-06-04 21:00:22 +0200

    bug

--- l10n_it_simple_withholding_cassa/__manifest__.py
+++ l10n_it_simple_withholding_cassa/__manifest__.py
@@ -6,14 +6,15 @@
     'category': 'Accounting',
     'summary': 'Aggiunge gestione ritenuta d\'acconto e cassa previdenziale nelle fatture e nelle offerte Odoo',
     'description': '''
-        - Applica ritenuta d’acconto nelle fatture clienti e offerte
+        - Applica ritenuta d'acconto nelle fatture clienti e offerte
         - Applica Cassa Previdenziale (es. INPS 4%) come riga automatica
         - I default possono essere definiti nei dati aziendali
-        - I dati selezionati nell’offerta vengono copiati nella fattura
+        - I dati selezionati nell'offerta vengono copiati nella fattura
     ''',
     'depends': [
         'account',
         'sale',
+        'portal',  # Aggiungi questa dipendenza
     ],
     'data': [
         'views/res_company_view.xml',
@@ -21,13 +22,10 @@
         'views/sale_order_view.xml',
         'views/report_saleorder_template.xml',
         'views/report_invoice_template.xml',
-        #'views/report_invoice_inherit.xml',
-        #'views/report_invoice_bank_details.xml',    # nuovo
-        'views/report_saleorder_bank_details.xml',  # nuovo
+        'views/report_saleorder_bank_details.xml',
         'views/assets.xml',
-        'views/portal_sale_order_templates.xml',  # Aggiungi questa riga
+        'views/portal_sale_order_templates.xml',  # Mantieni alla fine
     ],
     'installable': True,
     'application': False,
 }
-
